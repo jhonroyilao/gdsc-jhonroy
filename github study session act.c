637d5685decd0b70edf136e3186cf257a7415b3f
jhonroyilao <jhonroyilao@gmail.com>
2024-04-30 22:07:44 +0800

    first commit

---
+++ github study session act.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hi, I am Jhon Roy Ilao!\n");
+    printf("I am currently a freshman taking BSCS, from dash 2!\n");
+    printf("Let's Go Software Development Team!");
+
+    return 0;
+    }
